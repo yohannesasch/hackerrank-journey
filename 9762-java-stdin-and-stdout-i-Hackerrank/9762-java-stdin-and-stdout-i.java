5e7a072581382d75662cd4d7f8e38bdaa9234842
Yohannes <yohannesaschalew3@gmail.com>
2026-07-01 17:52:12 +0300

    Added solution - LeetHub

---
+++ 9762-java-stdin-and-stdout-i-Hackerrank/9762-java-stdin-and-stdout-i.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
